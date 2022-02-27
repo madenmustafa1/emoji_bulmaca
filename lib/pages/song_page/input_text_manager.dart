b3e739282c17ebd3f2f37970a80260543e1e5472
madenmustafa <madenn778@gmail.com>
2022-02-27 22:41:13 +0300

    update widgets

--- lib/pages/song_page/input_text_manager.dart
+++ lib/pages/song_page/input_text_manager.dart
@@ -1,17 +1,14 @@
 import 'package:emoji_bulmaca/model/input_text_model.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class InputTextManager extends StateNotifier<TextModel> {
   InputTextManager() : super(TextModel(""));
 
   void text({required String value}) {
-    debugPrint("text");
     state = TextModel(state.text);
   }
 
   void setText() {
-    debugPrint("settext");
     state = TextModel("");
   }
 }
